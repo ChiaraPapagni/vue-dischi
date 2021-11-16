f2f55dc720d50a98ad2dee43174cf6f04351fbda
ChiaraPapagni <chiaraiors@gmail.com>
2021-11-16 13:24:59 +0100

    add component selectGenre

---
+++ src/components/SelectGenre.vue
@@ -0,0 +1,31 @@
+<template>
+  <select
+    class="form-select"
+    name="genres"
+    id="genres"
+    v-model="selectedGenre"
+    @change="$emit('select-genre', selectedGenre)"
+  >
+    <option selected value="">Select the genre</option>
+    <option v-for="(genre, i) in genres" :key="i" :value="genre">
+      {{ genre }}
+    </option>
+  </select>
+  <!-- /.form-select -->
+</template>
+
+<script>
+export default {
+  props: {
+    genres: [],
+  },
+  data() {
+    return {
+      selectedGenre: "",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
